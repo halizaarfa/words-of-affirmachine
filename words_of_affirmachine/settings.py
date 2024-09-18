86756d13b296aba4b2d80d0e7550ef46cc79fa7e
halizaarfa <halizansa@gmail.com>
2024-09-18 10:59:51 +0700

    Tugas 3: Implementasi Form dan Data Delivery pada Django

--- words_of_affirmachine/settings.py
+++ words_of_affirmachine/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'words_of_affirmachine.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
