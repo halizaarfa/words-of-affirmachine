fbea09126e677fca74389931f8f2f9f8af6beae0
halizaarfa <halizansa@gmail.com>
2024-09-25 01:14:59 +0700

    Tugas 4: Implementasi Autentikasi, Session, dan Cookies pada Django

--- words_of_affirmachine/settings.py
+++ words_of_affirmachine/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yop4ppx!uvus5)vp2iif^ff$g4iszn@%v6$#7u@=+56*-=9a)*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "haliza-nafiah-wordsofaffirmachine.pbp.cs.ui.ac.id"]
 
