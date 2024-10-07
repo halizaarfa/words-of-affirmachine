b176d87334b3b42f277da3b9bd58205957493a0d
halizaarfa <halizansa@gmail.com>
2024-10-08 00:20:27 +0700

    Tugas 6: JavaScript dan AJAX - First trial

--- main/views.py
+++ main/views.py
@@ -13,15 +13,17 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
+
 @login_required(login_url='/login')
 def show_main(request):
-    products = Product.objects.filter(user=request.user)
 
     context = {
         'app' : 'Words of Affir-MACHINE',
         'name': request.user.username,
         'class': 'PBP C',
-        'products': products,
         'last_login': request.COOKIES['last_login'],
     }
 
@@ -39,12 +41,58 @@ def create_product(request):
     context = {'form': form}
     return render(request, "create_product.html", context)
 
+
+@csrf_exempt
+@require_POST
+def create_product_ajax(request):
+    name = strip_tags(request.POST.get("name"))
+    price = request.POST.get("price")
+    description = strip_tags(request.POST.get("description"))
+    stars = request.POST.get("stars")
+    user = request.user
+
+    new_product = Product(
+        name=name,
+        price=price,
+        description=description,
+        stars=stars,
+        user=user
+    )
+    new_product.save()
+
+    return HttpResponse(b"CREATED", status=201)
+
+def edit_product(request, id):
+    # Get product berdasarkan id
+    product = Product.objects.get(pk = id)
+
+    # Set product entry sebagai instance dari form
+    form = ProductForm(request.POST or None, instance=product)
+
+    if form.is_valid() and request.method == "POST":
+        # Simpan form dan kembali ke halaman awal
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_product.html", context)
+
+def delete_product(request, id):
+    # Get product berdasarkan id
+    product = Product.objects.get(pk = id)
+
+    # Hapus product
+    product.delete()
+
+    # Kembali ke halaman awal
+    return HttpResponseRedirect(reverse('main:show_main'))
+
 def show_xml(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
@@ -68,7 +116,7 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Your account has been successfully created!')
+            messages.success(request, 'account created!')
             return redirect('main:login')
     context = {'form':form}
     return render(request, 'register.html', context)
@@ -78,11 +126,15 @@ def login_user(request):
       form = AuthenticationForm(data=request.POST)
 
       if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            response = HttpResponseRedirect(reverse("main:show_main"))
-            response.set_cookie('last_login', str(datetime.datetime.now()))
-            return response
+        user = form.get_user()
+        login(request, user)
+        response = HttpResponseRedirect(reverse("main:show_main"))
+        response.set_cookie('last_login', str(datetime.datetime.now()))
+        return response
+      
+      else:
+        messages.error(request, "your data is invalid :( try again!")
+
 
    else:
       form = AuthenticationForm(request)
@@ -94,28 +146,3 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
-
-def edit_product(request, id):
-    # Get product berdasarkan id
-    product = Product.objects.get(pk = id)
-
-    # Set mood entry sebagai instance dari form
-    form = ProductForm(request.POST or None, instance=product)
-
-    if form.is_valid() and request.method == "POST":
-        # Simpan form dan kembali ke halaman awal
-        form.save()
-        return HttpResponseRedirect(reverse('main:show_main'))
-
-    context = {'form': form}
-    return render(request, "edit_product.html", context)
-
-def delete_product(request, id):
-    # Get product berdasarkan id
-    product = Product.objects.get(pk = id)
-
-    # Hapus mood
-    product.delete()
-
-    # Kembali ke halaman awal
-    return HttpResponseRedirect(reverse('main:show_main'))
